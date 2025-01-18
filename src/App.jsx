bc39800e66fe410ef2552b472740a0de4e9a6106
Chung1178 <allen117805@gmail.com>
2025-01-18 14:18:10 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,162 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+function App() {
+  const [account, setAccount] = useState({
+    username: '',
+    password: '',
+  });
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [tempProduct, setTempProduct] = useState({});
+  const [products, setProducts] = useState([]);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAccount({
+      ...account,
+      [name]: value,
+    });
+  };
+
+  const checkUserLogin = async () => {
+    try {
+      await axios.post(`${baseUrl}/v2/api/user/check`)
+      alert('使用者已登入');
+    } catch (error) {
+      alert('登入失敗，請重新登入')
+    }
+  }
+
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const apiPath = import.meta.env.VITE_API_PATH;
+
+  const handleLogin = async (e) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post(`${baseUrl}/v2/admin/signin`, account);
+      const { token, expired } = res.data;
+      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;
+      axios.defaults.headers.common['Authorization'] = token;
+      const productsData = await axios.get(`${baseUrl}/v2/api/${apiPath}/admin/products`);
+      setProducts(productsData.data.products);
+      setIsAuth(true);
+    } catch (error) {
+      alert(`登入失敗，失敗原因${error}`);
+    }
+  };
+
+  return (
+    <>
+      {isAuth ? (
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-6">
+              <button className='btn btn-success mb-5' onClick={checkUserLogin}>檢查使用者是否登入</button>
+              <h2>產品列表</h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">產品名稱</th>
+                    <th scope="col">原價</th>
+                    <th scope="col">售價</th>
+                    <th scope="col">是否啟用</th>
+                    <th scope="col">產品細節</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products.map((product) => (
+                    <tr key={product.id}>
+                      <th scope="row">{product.title}</th>
+                      <td>{product.origin_price}</td>
+                      <td>{product.price}</td>
+                      <td>{product.is_enabled ? '是' : '否'}</td>
+                      <td>
+                        <button
+                          onClick={() => {
+                            setTempProduct(product);
+                          }}
+                          className="btn btn-primary"
+                        >
+                          查看細節
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <div className="col-6">
+              <h2>單一產品細節</h2>
+              {tempProduct.title ? (
+                <div className="card">
+                  <img
+                    src={tempProduct.imageUrl}
+                    className="card-img-top img-fluid"
+                    alt="產品圖片"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {tempProduct.title}{' '}
+                      <span className="badge bg-primary">
+                        {tempProduct.category}
+                      </span>
+                    </h5>
+                    <p className="card-text">
+                      商品描述:{tempProduct.description}
+                    </p>
+                    <p className="card-text">商品內容:{tempProduct.content}</p>
+                    <p className="card-text">
+                      <del className="text-secondary">
+                        {tempProduct.origin_price}
+                      </del>{' '}
+                      元 / {tempProduct.price} 元
+                    </p>
+                    <h5 className="card-title">更多圖片: </h5>
+                    {tempProduct.imagesUrl?.map((imageUrl, index) => (
+                      <img className="img-fluid" src={imageUrl} key={index} />
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <p className="text-secondary">請選擇一個商品查看</p>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+          <h1 className="mb-5">請先登入</h1>
+          <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
+            <div className="form-floating mb-3">
+              <input
+                name="username"
+                type="email"
+                className="form-control"
+                id="username"
+                placeholder="name@example.com"
+                onChange={handleInputChange}
+              />
+              <label htmlFor="username">Email address</label>
+            </div>
+            <div className="form-floating">
+              <input
+                name="password"
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                onChange={handleInputChange}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button className="btn btn-primary">登入</button>
+          </form>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
